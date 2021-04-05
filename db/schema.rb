78f0f36605c3143a6c18bc0fda7a6993d1d8f2da
Javi <cappicecube@hotmail.com>
2021-04-05 13:39:57 +0100

    devise commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_04_05_120259) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "nombre", default: "", null: false
+    t.string "apellidos", default: "", null: false
+    t.string "telefono", default: "", null: false
+    t.string "cpostal", default: "", null: false
+    t.string "direccion", default: "", null: false
+    t.string "dni", default: "", null: false
+    t.string "pais", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_accounts_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
 
 end
