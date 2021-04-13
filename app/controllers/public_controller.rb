1ce4b6f6414e8e12b465a7857514177d93fc45ae
Alesa <alesaof@gmail.com>
2021-04-13 21:57:48 +0000

    filtro Marcas done

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,14 +1,19 @@
 class PublicController < ApplicationController
   def main
     @search = params[:search]
+    @selectmarca = params[:selectMarca]
     if @search
-      @products = Product.where("name lIKE ?","%#{@search}%")
-      @order_item = current_order.order_items.new
+      if @selectmarca == '0'
+        @products = Product.where("name lIKE ?","%#{@search}%")
+        @order_item = current_order.order_items.new
+      else
+        @products = Product.where(brand_id:@selectmarca)
+        @products = @products.where("name lIKE ?","%#{@search}%")
+        @order_item = current_order.order_items.new
+      end
     else
       @products = Product.all
       @order_item = current_order.order_items.new
     end
-    
   end
-  
 end
