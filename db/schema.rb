26015705cfee9ecf5bfb70d0b4987971fdaf1e7d
jesus <jelopezgonzalez@gmail.com>
2021-05-27 11:43:28 +0100

    heroku 3

--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,15 @@
 ActiveRecord::Schema.define(version: 2021_05_17_112201) do
 
   create_table "accounts", force: :cascade do |t|
-    t.string "nombre", null: false
-    t.string "apellidos", null: false
+    t.string "nombre", default: "", null: false
+    t.string "apellidos", default: "", null: false
     t.integer "telefono", null: false
     t.integer "cpostal", null: false
-    t.string "direccion", null: false
+    t.string "direccion", default: "", null: false
     t.integer "dni", null: false
-    t.string "pais", null: false
-    t.string "email", null: false
-    t.string "encrypted_password", null: false
+    t.string "pais", default: "", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
